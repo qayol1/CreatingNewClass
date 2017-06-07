0ea5fa3ac53cf36c498e4ac294eba365f7980a59
qayol1 <qayol@freemail.hu>
2017-06-07 14:56:32 +0200

    Person class created

---
+++ Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Person
+    {
+        enum Genders
+        {
+            Male,
+            Female
+        };
+
+        private String name;
+        private DateTime birthDate;
+        private Genders gender;
+
+        public override string ToString()
+        {
+            String result = "Name:" + name + ", Birth Date:" + birthDate.ToString("yyyy-MM-dd") + ", Gender:" + gender;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                name = "Kiss János",
+                birthDate = new DateTime(1974, 2, 9),
+                gender = Genders.Male
+            };
+            Console.WriteLine(person.ToString());
+            Console.ReadKey();
+        }
+    }
+}
